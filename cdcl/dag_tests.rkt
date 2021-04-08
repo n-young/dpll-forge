a98d91e2e0e6503054d608341086eb4046c93b91
DerickToth <57042699+DerickToth@users.noreply.github.com>
2021-04-07 21:42:14 -0400

    dag_test file added for inflection point testing

---
+++ cdcl/dag_tests.rkt
@@ -0,0 +1,19 @@
+#lang forge
+
+sig Atom {edges: set Atom}
+
+pred isDAG {
+    no iden & ^edges
+    Atom->Atom in ^(edges + ~edges)
+
+    one Atom - Atom.edges
+    one Atom - edges.Atom
+}
+
+pred hasInflection {
+    some a : Atom -(Atom - edges.Atom)-(Atom - Atom.edges){
+        no (Atom - edges.Atom) & (Atom - Atom.edges).^(edges & ((Atom-a) -> (Atom-a)))
+    }
+}
+
+run {isDAG and hasInflection} for exactly 7 Atom
